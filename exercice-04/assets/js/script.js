23ec087a3fa124e92ecb9766e4db1f5194a7d71e
Celine_lhp5 <celine.louvel.76@outlook.fr>
2020-11-06 11:18:41 +0100

    modif ok

--- exercice-04/assets/js/script.js
+++ exercice-04/assets/js/script.js
@@ -1,3 +1,29 @@
 function changeSrc() {
-    document.getElementById("lanterne").src=("osaka.jpg");
+    document.getElementById("lanterne").src=("assets/img/osaka.jpg");
 }
+
+// ou correction :
+
+// const image = document.getElementById("lanterne");
+
+// image.onclick = function changeSrc() {
+
+        // if (image.src.match("osaka.jpg") != null) {
+        //     image.src = "assets/img/lanterne.jpg";
+        // } else {
+        //     image.src = "assets/img/osaka.jpg";
+        // }
+
+// }
+
+
+
+// en remplacement du hover :
+
+// image.onmouseover = function () {
+//     image.style.width = "40rem";
+// }
+
+// image.onmouseleave = function () {
+//     image.style.width = "30rem";
+// }
---
+++ exercice-06/assets/js/script.js
@@ -0,0 +1,11 @@
+function addFocus() {
+    document.getElementById(name).focus();
+}
+
+function addfocus() {
+    document.getElementById(mail).focus();
+}
+
+function addFocus() {
+    document.getElementById(age).focus();
+}
