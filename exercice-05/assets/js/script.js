f923a26d538fe87ed3b9baf84e9d581989e03177
Celine_lhp5 <celine.louvel.76@outlook.fr>
2020-11-06 09:39:54 +0100

    modif ok

--- exercice-05/assets/js/script.js
+++ exercice-05/assets/js/script.js
@@ -1,3 +1,19 @@
 function rose() {
-    let pink = document.getElementById("pink").color: pink;
+    document.getElementById("text").style.color = "pink";
+}
+
+function blue() {
+    document.getElementById("text").style.color = "blue";
+}
+
+function purple() {
+    document.getElementById("text").style.color = "purple";
+}
+
+function bold() {
+    document.getElementById("text").style.fontWeight = "bold";
+}
+
+function italic() {
+    document.getElementById("text").style.fontStyle = "italic";
 }
