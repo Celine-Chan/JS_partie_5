76875fa435736db4f27e95f14546a033cdcbe257
Celine_lhp5 <celine.louvel.76@outlook.fr>
2020-11-09 09:46:39 +0100

    modif ok

--- exercice-05/assets/js/script.js
+++ exercice-05/assets/js/script.js
@@ -20,6 +20,31 @@ function italic() {
 
 
 
-// function changeColor(value) {
-//     document.getElementById("text").style.color = `${value}`;
+// correction :
+
+// const pText = document.getElementById("pText");
+// const pink = document.getElementById("pink");
+// const blue = document.getElementById("blue");
+// const purple = document.getElementById("purple");
+// const bold = document.getElementById("bold");
+// const italic = document.getElementById("italic");
+
+// pink.onclick = function(){
+//     pText.style.color = "pink";
+// }
+
+// blue.onclick = function(){
+//     pText.style.color = "blue";
+// }
+
+// purple.onclick = function(){
+//     pText.style.color = "purple";
+// }
+
+// bold.onclick = function(){
+//     pText.classList.toggle("bold");
+// }
+
+// italic.onclick = function(){
+//     pText.classList.toggle("italic");
 // }
