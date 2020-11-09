d6666e4b573519de8e10be842b1cb8e5e2b3b5d5
Celine_lhp5 <celine.louvel.76@outlook.fr>
2020-11-09 16:33:43 +0100

    modif ok

--- exercice-08/assets/js/script.js
+++ exercice-08/assets/js/script.js
@@ -1,4 +1,11 @@
-function cloneForm(){
+let formParent = document.getElementById("parentForm");
+let formBtn = document.getElementById("formButton");
 
+formBtn.onclick = duplicateForm;
 
+function duplicateForm(){
+    let formNode = document.getElementById("formNode");
+    let formNodeCopy = formNode.cloneNode(true);
+    
+    formParent.appendChild(formNodeCopy);
 }
